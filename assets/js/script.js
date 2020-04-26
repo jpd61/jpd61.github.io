f6ce2a91ff509cb3af69055dcaa3e7a0ec9ef99c
jpd61 <jpd@dewoodyoil.com>
2020-04-26 12:31:00 -0500

    refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,13 @@ Repository:
 https://github.com/jpd61/jpd61.github.io
 `);
 
-
+var searchTerm = "";
 
 var newsUrl = 'https://newsapi.org/v2/top-headlines?' +
           'country=us&' +
           'category=business&' +
           'apiKey=b25d20345a0649a68aca9e4e98da269d';
+
 var req = new Request(newsUrl);
 
 fetch(req)
@@ -20,25 +21,15 @@ fetch(req)
         return newsResponse.json();
     })
     .then((newsResponse) => {
-
-        for (i = 0; i < 5; i++) {
+        
+        for (let i = 0; i < 5; i++) {
+            let searchTerm = newsResponse.articles[i].title;
             let topNews = `   
-            <h6><a href="${newsResponse.articles[i].url}">${newsResponse.articles[i].title}</a></h6>
+            <h6><a href="${newsResponse.articles[i].url}">${searchTerm}</a></h6>
             `;
             $('#news').append(topNews);
 
-            // return fetch('https://api.giphy.com/v1/gifs/search?q=' +
-            // newsResponse.articles[i].title +
-            // '&api_key=Ig0KLAaZLoeBT9JfPAnR606tX2xozVrX')
-    
-            // .then((response) => {
-            //     return response.json();
-            //   })
-            // .then((response) => {
-            //     let gifImg = `<img src="${response.data[i].images.fixed_height.url}">`;
-            // })
-            // .then((response) => {
-            // $('#news').append(gifImg);
-            // })
+
+
     }
     })
