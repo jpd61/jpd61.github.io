786a234c4ef9240f8af0367896935a5adb97ef6e
jpd61 <jpd@dewoodyoil.com>
2020-04-25 21:52:17 -0500

    having trouble nesting giphy in the loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,17 +14,31 @@ var newsUrl = 'http://newsapi.org/v2/top-headlines?' +
           'category=business&' +
           'apiKey=b25d20345a0649a68aca9e4e98da269d';
 var req = new Request(newsUrl);
+
 fetch(req)
     .then((newsResponse) => {
         return newsResponse.json();
     })
     .then((newsResponse) => {
-        let searchTerm = newsResponse.articles[0].title
 
         for (i = 0; i < 5; i++) {
             let topNews = `   
             <h6><a href="${newsResponse.articles[i].url}">${newsResponse.articles[i].title}</a></h6>
-        `;
-        $('#news').append(topNews);
-        }
+            `;
+            $('#news').append(topNews);
+
+            // return fetch('https://api.giphy.com/v1/gifs/search?q=' +
+            // newsResponse.articles[i].title +
+            // '&api_key=Ig0KLAaZLoeBT9JfPAnR606tX2xozVrX')
+    
+            // .then((response) => {
+            //     return response.json();
+            //   })
+            // .then((response) => {
+            //     let gifImg = `<img src="${response.data[i].images.fixed_height.url}">`;
+            // })
+            // .then((response) => {
+            // $('#news').append(gifImg);
+            // })
+    }
     })
